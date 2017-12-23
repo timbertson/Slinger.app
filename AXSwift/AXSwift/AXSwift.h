ee4eea6c73e2ead5ffd90a051cbdcf1db3ae6cf0
Tim Cuthbertson <tim@gfxmonk.net>
2017-12-24 09:45:02 +1100

    .

---
+++ AXSwift/AXSwift/AXSwift.h
@@ -0,0 +1,19 @@
+//
+//  AXSwift.h
+//  AXSwift
+//
+//  Created by Tim Cuthbertson on 23/12/17.
+//  Copyright © 2017 Tim Cuthbertson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AXSwift.
+FOUNDATION_EXPORT double AXSwiftVersionNumber;
+
+//! Project version string for AXSwift.
+FOUNDATION_EXPORT const unsigned char AXSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXSwift/PublicHeader.h>
+
+
---
+++ Slinger/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  Slinger
+//
+//  Created by Tim Cuthbertson on 17/12/17.
+//  Copyright © 2017 Tim Cuthbertson. All rights reserved.
+//
+
+import Bullshit
